903dc9b1739a39568691be1349437b99d48c38be
Tim Hoer <timothy.hoer@duke.edu>
2017-09-07 16:17:39 -0400

    fixing all the problems I made, closes #1

---
+++ bme590lecture03/getting_testy.py
@@ -0,0 +1,40 @@
+import pytest
+def add(a, b):
+    if (type(a) or type(b)) is (int or float or double):
+        print("INVALID INPUT")
+    elif((a or b) < 0):
+        print 0
+        return 0
+    else:
+        c = a + b
+        print c
+        return c
+
+
+def testOne():
+    assert(add(-2, 1) == 0)
+
+def testTwo():
+    assert(add(1, 1) == 2)
+        
+def testThree():
+    assert(add(1, 1) == 2)
+        
+def testFour():
+    assert(add(3.1, 4.2) == 7.3) 
+        
+def testFive():
+    assert(add(3.1, 1) == 4.1)  
+        
+def testSix():
+    assert(add("3.1", 1) == "invalid input")  
+    
+testOne()
+testTwo()
+testThree()
+testFour()
+testFive()
+testSix()
+    
+
+
