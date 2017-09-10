57038e79e3be8b7a60ba9fc87e3ef28d3dba4b56
mv105 <mv105@duke.edu>
2017-09-10 03:12:51 -0400

    Modified working Code

--- bme590lecture03/getting_testy.py
+++ bme590lecture03/getting_testy.py
@@ -1,42 +1,37 @@
 import pytest
+
+
 def add(a, b):
-    a=input("enter 1st Number")
-    b=input("Enter 2nd Number")
-    if (type(a) or type(b)) is (int or float or double):
-        print("INVALID INPUT")
-    elif((a or b)<0):
-        print(0)
-        return 0
-    else:
-        c = a + b
+   
+
+    if(type(a) is int and type(b) is int and a > 0 and b > 0):
+        c=a+b
         print(c)
-        return c
+        return(c) 
+
+    else:
+        c=a+b
+        print("Invalid Input")
+        return "Invalid Input"
+def TestOne():
+    assert(add(-2,1) == "Invalid Input")
 
 
-def testOne():
-    assert(add(-2, 1) == 0)
-
-def testTwo():
-    assert(add(1, 1) == 2)
-        
-def testThree():
-    assert(add(1, 1) == 2)
-        
-def testFour():
-    assert(add(3.1, 4.2) == 7.3) 
-        
-def testFive():
-    assert(add(3.1, 1) == 4.1)  
-        
-def testSix():
-    assert(add("3.1", 1) == "invalid input")  
-    
-testOne()
-testTwo()
-testThree()
-testFour()
-testFive()
-testSix()
-    
+def TestTwo():
+    assert(add(1,1) == 2)
 
 
+def TestThree():
+    assert(add(1,1) ==2)
+
+
+TestOne()
+TestTwo()
+TestThree()
+
+
+
+
+a=int(input("Enter 1st Number"))
+b=int(input("Enter 2nd Number"))
+add(a,b)
